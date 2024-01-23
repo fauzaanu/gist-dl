1813b55939a7ff235a2e56c5d9f65da14e17d65e
fauzaanu <fauzaangasim@gmail.com>
2024-01-23 11:17:22 +0500

    Add Files

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#!/usr/bin/env python3
+import typer
+from gist.gist import Gist
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    gist = Gist()
+    gist.greeting()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    command = gist.get_command()
+    gist.process_command(command.split(" "))
+
+
+if __name__ == "__main__":
+    typer.run(main)
